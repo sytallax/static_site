59e5c1f0905ad7c78a723a731919b0aa4aa79507
sytallax <sytallax@gmail.com>
2024-06-09 02:24:24 -0500

    refactor: fix pylint issues

--- src/markdown_operations.py
+++ src/markdown_operations.py
@@ -18,55 +18,65 @@ class MarkdownFormattingError(Exception):
 
 def extract_markdown_images(text: str) -> List[Tuple[str, str]]:
     """Extract Markdown image information from a string."""
+
     return re.findall(
-        # Regex matches for Markdown images ( ![alt](link) )
-        r"!\[(.*?)\]\((.*?)\)",
+        r"!\[(.*?)\]\((.*?)\)",  # Regex matches for Markdown images ( ![alt](link) )
         text,
     )
 
 
 def extract_markdown_links(text: str) -> List[Tuple[str, str]]:
     """Extract Markdown link information from a string. Ignores images."""
+
+    text_sanitized = text
     image = re.search(r"!\[(.*?)\]\((.*?)\)", text)
     if image:
-        image_position = image.span()
-        text = text[: image_position[0]] + text[image_position[1] :]
+        image_position_in_text = image.span()
+        text_sanitized = (
+            text[: image_position_in_text[0]] + text[image_position_in_text[1] :]
+        )
     return re.findall(
-        # Regex matches for Markdown links ( [alt](link) )
-        r"\[(.*?)\]\((.*?)\)",
-        text,
+        r"\[(.*?)\]\((.*?)\)",  # Regex matches for Markdown links ( [alt](link) )
+        text_sanitized,
     )
 
 
 def markdown_to_blocks(markdown: str) -> List[str]:
-    """Separate a Markdown document into blocks. A Markdown block is
-    separated by an empty line, which is two newline characters."""
+    """Separate a Markdown document into blocks.
+
+    A Markdown block is separated by an empty line, which is represented
+    as two newline (\\n) characters.
+    """
+
     markdown_split_newline: List[str] = markdown.split("\n\n")
     markdown_split_newline_sanitized = [
-        x.strip().lstrip("\n") for x in markdown_split_newline if x
+        text.strip().lstrip("\n") for text in markdown_split_newline if text is not None
     ]
     return markdown_split_newline_sanitized
 
 
-def block_to_block_type(markdown_block: str):
-    """Calculate the type of block given"""
+def block_to_block_type(markdown_block: str) -> str:
+    """Calculate the type of Markdown block given."""
+
     if re.search(r"(?<!.)(#{1,6} )", markdown_block):
         return "heading"
     if re.search(r"^```", markdown_block) and re.search(r"```$", markdown_block):
         return "code"
     markdown_block_split = markdown_block.split("\n")
-    if all([">" == x[0] for x in markdown_block_split]):
+    if all(">" == x[0] for x in markdown_block_split):
         return "quote"
-    if all(["* " == x[:2] or "- " == x[:2] for x in markdown_block_split]):
+    if all("* " == x[:2] or "- " == x[:2] for x in markdown_block_split):
         return "unordered_list"
     is_block_ordered_list = True
-    for i in range(len(markdown_block_split)):
-        if f"{i+1}. " != markdown_block_split[i][:3]:
+    for i, block in enumerate(markdown_block_split):
+        if f"{i+1}. " != block[:3]:
             is_block_ordered_list = False
     return "ordered_list" if is_block_ordered_list else "paragraph"
 
 
 def markdown_to_html_node(markdown_document: str) -> HTMLNode:
+    """Convert an entire Markdown file into a large div HTMLNode."""
+
     markdown_blocks = markdown_to_blocks(markdown_document)
     markdown_blocks_and_types = [
         (block, block_to_block_type(block)) for block in markdown_blocks
@@ -80,13 +90,15 @@ def markdown_to_html_node(markdown_document: str) -> HTMLNode:
         "paragraph": _paragraph_to_html_node,
     }
     blocks_to_html_nodes = [
-        block_type_conversion_map[block[1]](block[0])
-        for block in markdown_blocks_and_types
+        block_type_conversion_map[block_type](block)
+        for block, block_type in markdown_blocks_and_types
     ]
     return ParentNode(blocks_to_html_nodes, "div")
 
 
 def _heading_to_html_node(block: str) -> HTMLNode:
+    """Convert a 'heading' Markdown block into an HTMLNode."""
+
     heading_text = block.lstrip("# ")
     heading_level = len(block) - len(heading_text) - 1
     textnodes = text_to_textnodes(heading_text)
@@ -97,17 +109,23 @@ def _heading_to_html_node(block: str) -> HTMLNode:
 
 
 def _code_to_html_node(block: str) -> HTMLNode:
+    """Convert a 'code' Markdown block into an HTMLNode."""
+
     code_text = block.strip("`").strip()
     code_node = LeafNode(code_text, "code")
     return ParentNode([code_node], "pre")
 
 
 def _quote_to_html_node(block: str) -> HTMLNode:
+    """Convert a 'quote' Markdown block into an HTMLNode."""
+
     quote_text = block.replace(">", "").replace("\n ", " ").strip()
     return LeafNode(quote_text, "blockquote")
 
 
 def _unordered_list_to_html_node(block: str) -> HTMLNode:
+    """Convert an unordered list in Markdown into an HTMLNode."""
+
     ul_children = []
     list_text = block.split("\n")
     list_text = [x[2:] for x in list_text]
@@ -123,6 +141,8 @@ def _unordered_list_to_html_node(block: str) -> HTMLNode:
 
 
 def _ordered_list_to_html_node(block: str) -> HTMLNode:
+    """Convert an ordered list in Markdown into an HTMLNode."""
+
     ol_children = []
     list_text = block.split("\n")
     list_text = [x[3:] for x in list_text]
@@ -138,6 +158,8 @@ def _ordered_list_to_html_node(block: str) -> HTMLNode:
 
 
 def _paragraph_to_html_node(block: str) -> HTMLNode:
+    """Convert a paragraph in Markdown into an HTMLNode."""
+
     block_sanitized = block.strip("\n").strip().replace("\n", " ")
     block_textnode = text_to_textnodes(block_sanitized)
     if len(block_textnode) == 1:
@@ -154,12 +176,16 @@ def _paragraph_to_html_node(block: str) -> HTMLNode:
     return ParentNode(block_as_html, "p")
 
 
-def extract_title(markdown_document: str):
+def extract_title(markdown_document: str) -> str:
+    """Return the title text of a Markdown file."""
+
     mardown_in_blocks = markdown_to_blocks(markdown_document)
     for block in mardown_in_blocks:
         if re.search(r"(?<!.)(# )", block):
             return block[2:]
-    raise Exception("Markdown document has no title")
+    raise MarkdownFormattingError(
+        markdown_text=markdown_document, message="Markdown document has no title"
+    )
 
 
 def split_nodes_delimiter(
@@ -186,7 +212,9 @@ def split_nodes_delimiter(
             TextNode(node_text, TextNodeType.TEXT)
             if node_split_by_delimiter.index(node_text) % 2
             == 0  # Regular TextNodes will be index 0 and 2
-            else TextNode(node_text, TextNodeType(text_type))  # New type node will be index 1
+            else TextNode(
+                node_text, TextNodeType(text_type)
+            )  # New type node will be index 1
             for node_text in node_split_by_delimiter
         ]
         output.extend([x for x in new_textnodes if x.text])
@@ -208,13 +236,13 @@ def split_nodes_image(old_nodes: List[TextNode]) -> List[TextNode]:
         if not images_in_node:
             output.append(node)
             continue
-        for index in range(0, len(images_in_node)):
-            alt = images_in_node[index][0]
-            link = images_in_node[index][1]
+        for i, image in enumerate(images_in_node):
+            alt = image[0]
+            link = image[1]
             node_with_image_removed: List[str] = node.text.split(f"![{alt}]({link})", 1)
             node_split.append(TextNode(node_with_image_removed[0], TextNodeType.TEXT))
             node_split.append(TextNode(alt, TextNodeType.IMAGE, link))
-            if index == len(images_in_node) - 1:
+            if i == len(images_in_node) - 1:
                 node_split.append(
                     TextNode(node_with_image_removed[1], TextNodeType.TEXT)
                 )
@@ -239,13 +267,13 @@ def split_nodes_link(old_nodes: List[TextNode]) -> List[TextNode]:
         if not links_in_node:
             output.append(node)
             continue
-        for index in range(0, len(links_in_node)):
-            alt = links_in_node[index][0]
-            link = links_in_node[index][1]
+        for i, link_data in enumerate(links_in_node):
+            alt = link_data[0]
+            link = link_data[1]
             node_text_split: List[str] = node.text.split(f"[{alt}]({link})", 1)
             node_split.append(TextNode(node_text_split[0], TextNodeType.TEXT))
             node_split.append(TextNode(alt, TextNodeType.LINK, link))
-            if index == len(links_in_node) - 1:
+            if i == len(links_in_node) - 1:
                 node_split.append(TextNode(node_text_split[1], TextNodeType.TEXT))
             else:
                 node = TextNode(node_text_split[1], TextNodeType.TEXT)
